048cce66e0683adbcbdabfe32371d748667550d3
senenpalanca97 <senen@trialing.org>
2022-02-28 16:33:41 +0100

    Configured for deployment

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://https://github.com/senenpalanca/profiles-rest-api'
+PROJECT_GIT_URL='https://github.com/senenpalanca/profiles-rest-api'
 
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
